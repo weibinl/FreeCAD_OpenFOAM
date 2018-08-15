16e5f6e8e95b9373ef10120f1e25a7227c514fd0
weibinl <liang.wei.bin@hotmail.com>
2018-08-15 14:52:14 -0400

    genereation of geo

--- FreeCAD/freecad_geometry.py
+++ FreeCAD/freecad_geometry.py
@@ -1,3 +1,13 @@
 from freecad_import import import_freecad_path
 FREECADPATH='/usr/lib/freecad/lib'
 import_freecad_path(FREECADPATH)
+
+FREECAD_DOC_NAME='python_scrit_test'
+FREECAD_DOC_PATH='/home/weibin/FREECAD_DOC/'
+FREECAD_DOC_EXTENSION='.fcstd' 
+# extension to use when sae freecad file
+
+WORKING_DOC=FreeCAD.newDocument(FREECAD_DOC_NAME)
+CYLINDER_1=WORKING_DOC.addObject('Part::Cylinder','CYLINDER_1')
+CYLINDER_1.Radius=4
+WORKING_DOC.saveAs(FREECAD_DOC_PATH+FREECAD_DOC_NAME+FREECAD_DOC_EXTENSION)
